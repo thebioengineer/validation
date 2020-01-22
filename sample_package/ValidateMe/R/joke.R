a575891cfb289948fbfee952b040caf6ed7796fc
thebioengineer <ellishughes@live.com>
2020-01-22 07:40:31 -0800

    add sample package;

---
+++ sample_package/ValidateMe/R/joke.R
@@ -0,0 +1,32 @@
+#' @title Deliver Jokes
+#' @description
+#'    Deliver jokes with punchlines. Wait 3 seconds for the punchline.
+#' @param Setup Joke setup
+#' @param Punchline Joke punchline
+#' @example
+#' joke('To the person who stole my presentation -','I hope you do not Excel.')
+#'
+#' @section Last updated by:
+#' Ellis Hughes
+#' @section Last updated date:
+#' 2020/01/29
+
+joke <- function(Setup, Punchline){
+  print(Setup)
+  Sys.sleep(3)
+  print(Punchline)
+}
+
+
+is_dad_joke<-function(x){
+  TRUE
+}
+
+caused_laugher <- function(x){
+  message("Did not cause laughter")
+  TRUE
+}
+
+embarrased_significant_other <- function(x){
+  TRUE
+}
---
+++ sample_package/ValidateMe/tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(ValidateMe)
+
+test_check("ValidateMe")
